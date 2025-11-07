7c9038d74f5ce7f1ac650bfd32e1beea0f812357
Shanyah Payne <payneshanyah226@gmail.com>
2025-11-07 01:38:38 -0400

    Edited the file to add some code

--- scripts.js
+++ scripts.js
@@ -1,1 +1,22 @@
 /* Add your JavaScript to this file */
+document.addEventListener('DOMContentLoaded', function() {
+    let emailInput = document.getElementById('email');
+    let form = document.querySelector('.newsletter form');
+    let messageDiv = document.querySelector('.newsletter .message');
+    
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        let email = emailInput.value.trim();
+
+        if (validateEmail(email)) {
+            messageDiv.textContent = "Thank you! Your email address "+ email + " has been added to our mailing list!.";
+            messageDiv.style.color = "green";
+            emailInput.value = '';
+        } else {
+            messageDiv.textContent = "Please enter a valid email address.";
+            messageDiv.style.color = "red";
+        }
+    });
+});
